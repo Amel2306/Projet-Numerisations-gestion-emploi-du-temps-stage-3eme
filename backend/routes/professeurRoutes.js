1cc54b04069d6cc501c52c8edffa102ef13ab5c8
Amel <ameladdou213@gmail.com>
2023-06-15 15:01:11 +0200

    modification routes

--- backend/routes/professeurRoutes.js
+++ backend/routes/professeurRoutes.js
@@ -6,6 +6,6 @@ router.get ('/', ProfesseurControllers.getAllProfesseurs);
 router.get ('/tuteur/:tuteurId', ProfesseurControllers.getEleveByTuteur)
 router.post('/', ProfesseurControllers.addProfesseur);
 router.delete('/:id', ProfesseurControllers.deleteProfesseur);
-router.delete('/all', ProfesseurControllers.deleteAllProfesseurs)
+router.delete('/', ProfesseurControllers.deleteAllProfesseurs)
 
 module.exports = router; 
